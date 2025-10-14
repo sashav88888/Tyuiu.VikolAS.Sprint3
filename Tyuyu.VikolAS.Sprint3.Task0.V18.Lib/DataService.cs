5cabff987abc1a281cf43c1e6637c45f9ea6f438
alex vikol <sashavikol7@gmail.com>
2025-10-14 14:58:33 +0500

    Add project files.

---
+++ Tyuyu.VikolAS.Sprint3.Task0.V18.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuyu.VikolAS.Sprint3.Task0.V18.Lib
+
+{
+    public class DataService : ISprint3Task0V18
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double p = 1;
+
+            for (int k = startValue; k <= stopValue; k++)
+            {
+                p *= Math.Pow(2 / (Math.Cos(value) + 0.5), k);
+            }
+
+            return Math.Round(p, 3);
+        }
+    }
+}
---
+++ Tyuyu.VikolAS.Sprint3.Task0.V18/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuyu.VikolAS.Sprint3.Task0.V18.Lib;
+namespace Tyuiu.VikolAS.Sprint3.Task0.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = ("Спринт #2 | Выполнил: Викол А.С. | ИСПб-25-1   ");
+            Console.WriteLine("******************************************   *");
+            Console.WriteLine("* Спринт #3                                  *");
+            Console.WriteLine("* Тема: Оператор цикла for                   *");
+            Console.WriteLine("* Задание #0                                 *");
+            Console.WriteLine("* Вариант #18                                 *");
+            Console.WriteLine("* Выполнил: Викол А.С. | ИСПб-25-1           *");
+            Console.WriteLine("******************************************   *");
+            Console.WriteLine("* УСЛОВИЕ :                                   *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение ряда по формуле, при X=1 *");
+            Console.WriteLine("*****************************************    *");
+
+            int x = 1;
+            int start = 1;
+            int stop = 6;
+
+            double result = ds.GetMultiplySeries(x, start, stop);
+
+            Console.WriteLine($"Результат вычисления p = {result}");
+            Console.ReadKey();
+        }
+    }
+}
