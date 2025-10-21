eee0de1fe403e074cacc5681829f177958349aef
alex <sashavikol7@gmail.com>
2025-10-21 14:08:03 +0500

    Update DataService.cs

--- Tyuiu.VikolAS.Sprint3.Task0.V18.Lib/DataService.cs
+++ Tyuiu.VikolAS.Sprint3.Task0.V18.Lib/DataService.cs
@@ -1,20 +1,25 @@
+using System;
 using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.VikolAS.Sprint3.Task0.V18.Lib
 {
+    // Интерфейс для задания (можно подключить, если нужен)
+    public interface ISprint3Task0V18
+    {
+        double GetMultiplySeries(int value, int startValue, int stopValue);
+    }
+
     public class DataService : ISprint3Task0V18
     {
         public double GetMultiplySeries(int value, int startValue, int stopValue)
         {
             double p = 1;
 
-            // Цикл от startValue до stopValue включительно
             for (int k = startValue; k <= stopValue; k++)
             {
-                // Вычисляем произведение ряда по формуле p *= (2 / (cos(X) + 0.5))^k
+                // Формула произведения ряда
                 p *= Math.Pow(2 / (Math.Cos(value) + 0.5), k);
             }
 
-            // Округляем результат до 3 знаков после запятой
             return Math.Round(p, 3);
         }
     }
