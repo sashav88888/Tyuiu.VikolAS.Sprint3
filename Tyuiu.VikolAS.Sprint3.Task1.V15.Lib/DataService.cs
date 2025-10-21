be82a21786587f32fd53a9acb016d4dc29b4cbe5
alex vikol <sashavikol7@gmail.com>
2025-10-21 10:26:18 +0500

    Task1

---
+++ Tyuiu.VikolAS.Sprint3.Task1.V15.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.VikolAS.Sprint3.Task1.V15.Lib
+{
+    public class DataService : ISprint3Task1V15
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double p = 1;
+            int i = startValue;
+            while (i <= stopValue)
+            {
+                p *= (Math.Pow(value, 2) * i);
+                i++;
+            }
+            p = p + 1;
+            return Math.Round(p, 3);
+        }
+    }
+}
---
+++ Tyuiu.VikolAS.Sprint3.Task1.V15.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.VikolAS.Sprint3.Task1.V15.Lib;
+
+namespace Tyuiu.VikolAS.Sprint3.Task1.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int x = 2;
+            int startValue = 1;
+            int stopValue = 5;
+
+            double expected = 1;
+            int i = startValue;
+            while (i <= stopValue)
+            {
+                expected *= (Math.Pow(x, 2) * i);
+                i++;
+            }
+            expected = Math.Round(expected + 1, 3);
+
+            double result = ds.GetMultiplySeries(x, startValue, stopValue);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
