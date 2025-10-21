7cea03e95269514f44bc831acd4607668c5e879f
alex <sashavikol7@gmail.com>
2025-10-21 14:05:02 +0500

    Update DataService.cs

--- Tyuiu.VikolAS.Sprint3.Task0.V18.Lib/DataService.cs
+++ Tyuiu.VikolAS.Sprint3.Task0.V18.Lib/DataService.cs
@@ -1,6 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint3;
+using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.VikolAS.Sprint3.Task0.V18.Lib
-
 {
     public class DataService : ISprint3Task0V18
     {
@@ -8,13 +7,15 @@ namespace Tyuiu.VikolAS.Sprint3.Task0.V18.Lib
         {
             double p = 1;
 
+            // Цикл от startValue до stopValue включительно
             for (int k = startValue; k <= stopValue; k++)
             {
+                // Вычисляем произведение ряда по формуле p *= (2 / (cos(X) + 0.5))^k
                 p *= Math.Pow(2 / (Math.Cos(value) + 0.5), k);
             }
 
+            // Округляем результат до 3 знаков после запятой
             return Math.Round(p, 3);
-
         }
     }
 }
